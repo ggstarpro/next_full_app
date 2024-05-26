2448250f9afc5368223725fe30f353d2ccbf096c
GentaMorino <genta.morino@gmail.com>
2024-05-26 21:57:23 +0900

    ローディングコンポーネント

---
+++ src/app/(main)/task/loading.tsx
@@ -0,0 +1,14 @@
+/**
+ * 非同期処理を行っている間に表示されるコンポーネント
+ * ファイル名はloading.tsxとする必要がある
+ */
+
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>loading</div>
+  )
+}
+
+export default loading
--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -12,7 +12,11 @@ const tasks: Task[] = [
   {id:2, name: "typescript"},
 ];
 
+const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms))
+
 export const GET = async () => {
+  await sleep(3000);
+
   return NextResponse.json({ tasks }, {
     status: 200,
   });
